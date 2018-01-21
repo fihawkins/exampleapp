18c5fc53e5826bcf03cc4c7c5951d568c86f96ab
fihawkins <filipe.ufabc@gmail.com>
2018-01-21 10:06:50 -0200

    http response messages

--- ExampleApp/ExampleApp/Controllers/ProductsController.cs
+++ ExampleApp/ExampleApp/Controllers/ProductsController.cs
@@ -1,5 +1,8 @@
-﻿using ExampleApp.Models;
+﻿using ExampleApp.Infrastructure;
+using ExampleApp.Models;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 
 namespace ExampleApp.Controllers
@@ -18,9 +21,17 @@ namespace ExampleApp.Controllers
             return repo.Products;
         }
 
-        public void Delete(int id)
+        public IHttpActionResult Delete(int id)
         {
             repo.DeleteProduct(id);
+            return new NoContentResult();
+        }
+
+        [HttpGet]
+        [Route("api/products/noop")]
+        public IHttpActionResult NoOp()
+        {
+            return Ok();
         }
     }
 }
